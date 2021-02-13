a951e67c7064d92c9540836736d5d0caab5a482d
James <james@jamesmcc.co.uk>
2021-02-13 17:24:04 +0000

    Started to Add the /list route

--- app/main.py
+++ app/main.py
@@ -8,7 +8,10 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/list")
+def get_list():
+    return {}
